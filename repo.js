d2652f2083b388a0004487d821b4f8975dab349e
Some Guy in Paradise <tomvilar11937@gmail.com>
2020-08-11 08:43:28 -0400

    commit

--- repo.js
+++ repo.js
@@ -3,6 +3,7 @@
     "id": "someguy",
     "patches": {
         "aya_bad": "More like, whya! Makes Aya harder than what she was originally. Otherwise a test mod."
+	    "kaizo_ddc": "A remodeling of DDC to be harder and more interesting."
     },
     "servers": [
         "https://mirrors.thpatch.net/someguy/"
