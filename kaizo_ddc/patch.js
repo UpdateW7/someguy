d6fb62109d8281e1517aebcebdc302158b0303fd
Some Guy in Paradise <tomvilar11937@gmail.com>
2020-08-11 09:00:59 -0400

    Kaizo DDC!

---
+++ kaizo_ddc/patch.js
@@ -0,0 +1,7 @@
+{
+    "id": "kaizo_ddc",
+    "servers": [
+        "https://mirrors.thpatch.net/someguy/"
+    ],
+    "title": "A remodeling of DDC to be harder and more interesting."
+}
---
+++ kaizo_ddc/th14/musiccmt.js
@@ -0,0 +1,171 @@
+﻿{
+    "1": [
+        "@",
+        "　",
+        "The title screen theme. (Original: \"Mystery\" by maritumix)",
+        "",
+        "Hello! I'm this is a message from the dev of this mod, I changed the title theme here.",
+        "Don't get too excited. I only changed this and the results theme.",
+        "No need to check your sound system, that's how the song sounds! I just think it sounds nice.",
+        "(Result theme origin: \"輝く針の小人族 ～ Little Princess\" by 37467524)"
+    ],
+    "2": [
+        "@",
+        "　",
+        "The stage 1 theme.",
+        "",
+        "I'll just give my own comments of each song, while I'm here.",
+        "I like it. One of the best stage 1 themes. Upbeat, but no where near silly.",
+        "Cirno's here too.",
+        "She kinda missed the kaizo memo."
+    ],
+    "3": [
+        "@",
+        "　",
+        "Wakasagihime's theme.",
+        "",
+        "Whaddya mean the guitars ruin it? I like those!",
+        "The sax/flute thing at the refrain makes the song, though.",
+        "It makes you kinda think \"Let's fry this fool to some sushi!\"",
+        "California rolls are the best!"
+    ],
+    "4": [
+        "@",
+        "　",
+        "The stage 2 theme.",
+        "",
+        "It's around here where you learn my love for bubble bullets. They're big and strong.",
+        "I wonder why ZUN doesn't use them as often, they convey some much power to the attacks.",
+        "",
+        "A bit short of a song though."
+    ],
+    "5": [
+        "@",
+        "　",
+        "Sekibanki's theme.",
+        "",
+        "You ever just have your head fly off?",
+        "Me too.",
+        "Probably the best early game song in my opinion.",
+        "Had this stuck for sometime when I was \"kaizoing\" Sekibanki."
+    ],
+    "6": [
+        "@",
+        "　",
+        "The stage 3 theme.",
+        "",
+        "Ah, the Bamboo Forest. I had no idea I was playing a slightly better game.",
+        "Stop saying this has the leitmotif from  <i$Imperishable Night>'s stage 4. It's different here!",
+        "Overall, good song. The music and stage cues are on point here, looping once the boss shows up.",
+        "I wish other games do such a thing."
+    ],
+    "7": [
+        "@",
+        "　",
+        "Kagerou Imaizumi's theme.",
+        "",
+        "No! You can't just spam the wolf sound and make her Aya fast! That's just lazy and annoying! NOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO",
+        "",
+        "haha wolf go aooaooaooaooaooaoo",
+        ""
+    ],
+    "8": [
+        "@",
+        "　",
+        "The stage 4 theme.",
+        "",
+        "Difficulty spikes, am I right?",
+        "I like this one. Definitely conveys the late game feel.",
+        "As you can see while playing, lasers are fun.",
+        "Tip: In Lunatic, <i$don't move> for the 2nd type of lasers that show up before the tile card and midboss."
+    ],
+    "9": [
+        "@",
+        "　",
+        "Benben Tsukumo's & Yatsuhashi Tsukumo's theme.",
+        "",
+        "Banger. Made the most annoying parts of making the boss fight that much more bareable.",
+        "By now, you should be expecting some sort of fast, less dense attack here and there.",
+        "And yes, I <i$did> port a piece from <i$Impossible Spell Card>.",
+        "Fun, isn't it?"
+    ],
+    "10": [
+        "@",
+        "　",
+        "The stage 5 theme.",
+        "",
+        "Best stage 5 theme in the series. Fight me.",
+        "I originally planned on replacing this with \"Inversion\" by maritumix,",
+        "but I scrapped it. Music cues.",
+        ""
+    ],
+    "11": [
+        "@",
+        "　",
+        "Seija Kijin's theme.",
+        "",
+        "Flip! You will now have <b$rotten day>",
+        "",
+        "",
+        ""
+    ],
+    "12": [
+        "@",
+        "　",
+        "The stage 6 theme.",
+        "",
+        "Leitmotifs are the greatest, aren't they?",
+        "Why this song was near the bottom of the 2020 Popularity Poll (390 out of 540), I have no idea.",
+        "It honestly sets the mood so much for the final boss.",
+        "Trust me, the payoff for the mood is huge."
+    ],
+    "13": [
+        "@",
+        "　",
+        "Shinmyoumaru Sukuna's theme.",
+        "",
+        "My favorite song in the series, no doubt. The pacing of the song is absolutly flawless. ",
+        "However, there's a humongous issue. The climax of the song happened at the",
+        "most boring penultimate card ever. That's indirectly why I made this mod.",
+        "Jeez, I am petty."
+    ],
+    "14": [
+        "@",
+        "　",
+        "The extra stage theme.",
+        "",
+        "Very \"Extra\"-like, in an imposing sense.",
+        "I think the opening Romantic Tips are a leitmotif for Raiko. Is that just me?",
+        "The midboss is the <i$worst>, but the music and stage's good.",
+        ""
+    ],
+    "15": [
+        "@",
+        "　",
+        "Raiko Horikawa's theme.",
+        "",
+        "Drum go bang bang bang. I like this one, very pacing and very impacting.",
+        "Did you know, if you are lucky enough, you can have a phase of Raiko's ",
+        "final attack exactly on beat? I wish for a mod that makes that always happen.",
+        "I have yet to beat Raiko as of the making of this mod, though. (^^')"
+    ],
+    "16": [
+        "@",
+        "　",
+        "The ending theme.",
+        "",
+        "Leitmotif.",
+        "",
+        "That is all."
+    ],
+    "17": [
+        "@",
+        "　",
+        "The staff roll theme.",
+        "",
+        "Apparently this has the title theme leitmotif.",
+        "That's cool.",
+        "",
+        ""
+    ]
+}
