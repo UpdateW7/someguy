8dbea52daa834f9d5f7a9dd8375e2f374d6922e1
Some Guy in Paradise <tomvilar11937@gmail.com>
2020-10-10 14:02:35 -0400

    Ringo

---
+++ ringo/patch.js
@@ -0,0 +1,12 @@
+{
+    "id": "ringo",
+    "dependencies": [
+        "nmlgc/base_tsa",
+        "nmlgc/script_latin",
+        "nmlgc/western_name_order"
+    ],
+    "servers": [
+        "https://mirrors.thpatch.net/someguy/"
+    ],
+    "title": "Not the starr, the bunny. Replaces TH15 Junko with a custom Ringo boss fight. Lots of bugs regarding music."
+}
---
+++ ringo/th15/musiccmt.js
@@ -0,0 +1,171 @@
+{
+    "1": [
+        "@",
+        "　",
+        "The title screen theme. ",
+        "",
+        "This theme turned out a bit spacey and Japanese. ",
+        "For this game, the excitement factor could be described as \"unconventional.\""
+    ],
+    "3": [
+        "@",
+        "　",
+        "Seiran's theme.",
+        "",
+        "It's bringing out the feeling of having fun with a light-hearted battle.",
+        "Rabbits don't seem to have it in their nature to be serious.",
+        "",
+        "You can't see it, but she does have a tail."
+    ],
+    "2": [
+        "@",
+        "　",
+        "The first stage theme.",
+        "",
+        "This song is a bit long for the first stage.",
+        "The game is pretty long as a whole, so I designed it to match.",
+        "",
+        "It feels like they are making up their mind to leave their beautiful green Earth for a while."
+    ],
+    "4": [
+        "@",
+        "　",
+        "The second stage theme.",
+        "",
+        "Making us feel uneasy with the changing situation, this song became one",
+        "that wouldn't feel out-of-place in the latter half of the game.",
+        "",
+        "It seems like only a select few can see the pulse given off by the Moon."
+    ],
+    "5": [
+        "@",
+        "　",
+        "Ringo's theme.",
+        "",
+        "Unlike Seiran, she's a rabbit who works in the background rather than a soldier.",
+        "On top of being a powerful opponent, this song depicts her doubts as to whether or not",
+        "fighting is really the right thing to do.",
+        "",
+        "All those dango were enjoyed by the TV staff afterward, of course."
+    ],
+    "6": [
+        "@",
+        "　",
+        "The third stage theme.",
+        "",
+        "It looks like space, but it's actually the mental world. So it is an \"all just a dream\" twist, in a sense.",
+        "The fairies and phantoms wandering around are the dreams of said creatures in reality.",
+        "",
+        "It's mischievous of them to spread barrages of danmaku even in dreams."
+    ],
+    "7": [
+        "@",
+        "　",
+        "Doremy Sweet's theme.",
+        "",
+        "It gives off a strong sense of foreboding.",
+        "I wrote this with thoughts of \"I want you to be wary from here on out,\"",
+        "\"I want you to have a nightmare,\" and so on.",
+        "",
+        "Even if you have a nightmare, it'll be fine if you pray to change it. Pray to Doremy."
+    ],
+    "8": [
+        "@",
+        "",
+        "The fourth stage theme.",
+        "",
+        "It's the Lunar Capital. For a particular reason, however, it's now a ghost town.",
+        "It's a song with a calm atmosphere, but on the contrary, it evokes an image of madness.",
+        "",
+        "It's music more befitting the endgame of an RPG than a shooting game. Well, it is the endgame."
+    ],
+    "9": [
+        "@",
+        "　",
+        "Kishin Sagume's theme.",
+        "",
+        "A theme song for a mysterious person who's not quite an enemy or an ally, a god or an oni.",
+        "Hence, I composed it with a rather mysterious mood.",
+        "",
+        "Actually, she's a type of amanojaku too so that makes her the second one."
+    ],
+    "10": [
+        "@",
+        "　",
+        "The fifth stage theme.",
+        "",
+        "Some minor variations to the mood make this a fast-paced, romantic tune.",
+        "It's a stage where you can move forward (and rack up a huge score) at a good pace.",
+        "Feels real nice.",
+        "I hope you feel a sense of flight, and the resolve to travel far."
+    ],
+    "11": [
+        "@",
+        "　",
+        "Clownpiece's theme.",
+        "",
+        "She's the most crazy-strong fairy seen yet. Although she's pretty ordinary in the head.",
+        "Having made it gothic, Japanese, and spacey at the same time, this is a pretty odd song.",
+        "The fear of clowns is pretty widespread these days.",
+        "But this fairy clown isn't all that scary... right?"
+    ],
+    "12": [
+        "@",
+        "　",
+        "The sixth stage theme.",
+        "",
+        "It's the seaaa. It's summeeerr.",
+        "",
+        "This song is a subset of the final boss' theme.",
+        "I want to go to the seaaa. I want to drink beer in a beach huuut."
+    ],
+    "13": [
+        "@",
+        "　",
+        "Ringo's second theme.",
+        "(Source: \"The Rabbit of the Unpeaceful Banquet\" by しゃわ)",
+        "(Phase 2 source: \"The Moon Rabbit and the Forbidden Fruit ~ Eden's Apple\" by maceknight)",
+        "Hi, this is the developer, and you are hearing the only reason I made this.",
+        "This and the phase 2 music was my strength and downfall.",
+        "Just play on Pointdevice or with continues, and you'll see why."
+    ],
+    "14": [
+        "@",
+        "　",
+        "The Extra Stage theme.",
+        "",
+        "First the eerie intro, then it flows into the simple and usual Touhou style.",
+        "It really does have that Extra feel.",
+        "Nonetheless, is Doremy your foe this time?",
+        "Or is she your friend?"
+    ],
+    "15": [
+        "@",
+        "　",
+        "Hecatia Lapislazuli's theme.",
+        "",
+        "This is quite a demonic piece of music.",
+        "I actually thought about making a metal song, but I'm not well-versed in that area",
+        "and it felt like it wouldn't fit the setting, so I made it like this in the end.",
+        "My thought process was Hell -> Shinigami -> Death -> Death Metal. Pretty poor idea."
+    ],
+    "16": [
+        "@",
+        "　",
+        "The ending theme.",
+        "",
+        "Well, it's the usual theme.",
+        "",
+        "Because I usually write songs that are always upbeat,",
+        "I feel like novel game music and such must be a real challenge."
+    ],
+    "17": [
+        "@",
+        "　",
+        "The staff roll theme.",
+        "",
+        "It's an alternate version of the title screen song.",
+        "",
+        "I gave it an even spacier mood than before."
+    ]
+}
