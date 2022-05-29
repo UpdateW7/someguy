85eae7e60f1cb17f87d3e4c4752f48b13a19467a
Some Guy in Paradise <tomvilar11937@gmail.com>
2022-05-29 13:29:47 -0400

    text fix that hopefully doesn't break sprites and stuff

--- vdr/patch.js
+++ vdr/patch.js
@@ -2,6 +2,7 @@
 	"vdr": "A remodel of VD because itself was garbage.",
     "title": "A scrapbook of ideas and messing around with the game. Fun stuff.",
     "dependencies": [
+        "thpatch/lang_en",
         "nmlgc/base_tsa"
     ],
     "update": true,
