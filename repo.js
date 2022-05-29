09a03f56e2a843a2b0cb40fdeacbfa1a927d8b51
Some Guy in Paradise <tomvilar11937@gmail.com>
2022-05-29 12:59:08 -0400

    Minor Spelling Mistake

--- repo.js
+++ repo.js
@@ -11,8 +11,8 @@
 		"nohead": "Replaces TH14 Sukuna with a custom Sekibanki boss fight. I watched one boss rush video, and this happened.",
 		"umdemo": "A medley of the first 3 stages of UM crafted within HSiFS because why not? Kinda rushed for a contest, but I tried.",
 		"umalt": "A scrapbook of ideas and messing around with the game. Fun stuff.",
-		"epro": "A danmaku mockup showcase for a Stage 1 and 6 of fangame minus the game. Made in pure fun."
-	    "vdr": "A remodel of VD because itself was garbage.",
+		"epro": "A danmaku mockup showcase for a Stage 1 and 6 of fangame minus the game. Made in pure fun.",
+	    "vdr": "A remodel of VD because itself was garbage."
     },
     "servers": [
         "https://mirrors.thpatch.net/someguy/"
