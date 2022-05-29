a9585822a51b2ae12fd0d079a758cdceb948fe0a
Some Guy in Paradise <tomvilar11937@gmail.com>
2022-05-29 13:13:44 -0400

    scorefilenew

--- repo.js
+++ repo.js
@@ -1,21 +1,21 @@
 {
-    "contact": "Some Guy in Paradise#2170",
-    "id": "someguy",
-    "patches": {
-        "aya_bad": "More like, whya! Makes Aya harder than what she was originally. Otherwise a test mod.",
-	    "kaizo_ddc": "A remodeling of DDC to be harder and more interesting.",
-		"funny_fish": "A bonus stage for KDDC starring a certain funny fish.",
-        "ringo": "Not the starr, the bunny. Replaces TH15 Junko with a custom Ringo boss fight. Lots of bugs regarding music.",
-	    "kaizo_wbawc": "A remodel of WBaWC, because Kaizo DDC was garbage.",
-	    "morpho": "An After Extra of HSiFS made by me because Eternity is a cool character. ",
-		"nohead": "Replaces TH14 Sukuna with a custom Sekibanki boss fight. I watched one boss rush video, and this happened.",
-		"umdemo": "A medley of the first 3 stages of UM crafted within HSiFS because why not? Kinda rushed for a contest, but I tried.",
-		"umalt": "A scrapbook of ideas and messing around with the game. Fun stuff.",
+	"contact": "Some Guy in Paradise#2170",
+	"id": "someguy",
+	"patches": {
+		"aya_bad": "More like, whya! Makes Aya harder than what she was originally. Otherwise a test mod.",
 		"epro": "A danmaku mockup showcase for a Stage 1 and 6 of fangame minus the game. Made in pure fun.",
-	    "vdr": "A remodel of VD because itself was garbage."
-    },
-    "servers": [
-        "https://mirrors.thpatch.net/someguy/"
-    ],
-    "title": "Bullet Artistry"
+		"funny_fish": "A bonus stage for KDDC starring a certain funny fish.",
+		"kaizo_ddc": "A remodeling of DDC to be harder and more interesting.",
+		"kaizo_wbawc": "A remodel of WBaWC, because Kaizo DDC was garbage.",
+		"morpho": "An After Extra of HSiFS made by me because Eternity is a cool character.",
+		"nohead": "Replaces TH14 Sukuna with a custom Sekibanki boss fight. I watched one boss rush video, and this happened.",
+		"ringo": "Not the starr, the bunny. Replaces TH15 Junko with a custom Ringo boss fight. Lots of bugs regarding music.",
+		"umalt": "A scrapbook of ideas and messing around with the game. Fun stuff.",
+		"umdemo": "A medley of the first 3 stages of UM crafted within HSiFS because why not? Kinda rushed for a contest, but I tried.",
+		"vdr": "A scrapbook of ideas and messing around with the game. Fun stuff."
+	},
+	"servers": [
+		"https://mirrors.thpatch.net/someguy/"
+	],
+	"title": "Bullet Artistry"
 }
