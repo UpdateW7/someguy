744fbb8266a2031f9889a375855d5bd4b081de03
Some Guy in Paradise <tomvilar11937@gmail.com>
2022-08-21 19:15:08 -0400

    nyoom

---
+++ dynastg/patch.js
@@ -0,0 +1,10 @@
+{
+    "id": "nitroni",
+    "dependencies": [
+        "nmlgc/base_tsa"
+    ],
+    "servers": [
+        "https://mirrors.thpatch.net/someguy/"
+    ],
+    "title": "Cool Nitori spriteswap retexture thingy for 18.5."
+}
