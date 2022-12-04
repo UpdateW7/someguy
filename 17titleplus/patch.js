c61250fc92ee074926b336256ce4ac3efaf751bc
Some Guy in Paradise <tomvilar11937@gmail.com>
2022-12-03 19:52:34 -0500

    WBaWC is cool

---
+++ 17titleplus/patch.js
@@ -0,0 +1,11 @@
+{
+    "id": "17titleplus",
+    "title": "A multilingually supported patch that adds a cool big graphic onto the stage titles.",
+    "dependencies": [
+        "nmlgc/base_tsa"
+    ],
+    "update": true,
+    "servers": [
+        "https://mirrors.thpatch.net/someguy/"
+    ]
+}
--- repo.js
+++ repo.js
@@ -3,20 +3,21 @@
 	"id": "someguy",
 	"patches": {
 		"aya_bad": "More like, whya! Makes Aya harder than what she was originally. Otherwise a test mod.",
+		"cirnoday": "A celebration for Cirno! Adds to Cirno's 100BM moveset.",
+		"dynastg": "18.5 backgrounds go nyoom and wheee per wave.",
 		"epro": "A danmaku mockup showcase for a Stage 1 and 6 of fangame minus the game. Made in pure fun.",
 		"funny_fish": "A bonus stage for KDDC starring a certain funny fish.",
 		"kaizo_ddc": "A remodeling of DDC to be harder and more interesting.",
 		"kaizo_wbawc": "A remodel of WBaWC, because Kaizo DDC was garbage.",
 		"morpho": "An After Extra of HSiFS made by me because Eternity is a cool character.",
+		"nitroni": "Cool Nitori spriteswap retexture thingy for 18.5.",
 		"nohead": "Replaces TH14 Sukuna with a custom Sekibanki boss fight. I watched one boss rush video, and this happened.",
 		"ringo": "Not the starr, the bunny. Replaces TH15 Junko with a custom Ringo boss fight. Lots of bugs regarding music.",
 		"umalt": "A scrapbook of ideas and messing around with the game. Fun stuff.",
 		"umdemo": "A medley of the first 3 stages of UM crafted within HSiFS because why not? Kinda rushed for a contest, but I tried.",
-		"vdr": "A scrapbook of ideas and messing around with the game. Fun stuff.",
 		"vdlc": "An add-on for vdr that changes a few characters around for funsies. Changes the 3rd Friday and Final Day Scene 3.",
-		"nitroni": "Cool Nitori spriteswap retexture thingy for 18.5.",
-		"dynastg": "18.5 backgrounds go nyoom and wheee per wave.",
-		"cirnoday": "A celebration for Cirno! Adds to Cirno's 100BM moveset."
+		"vdr": "A scrapbook of ideas and messing around with the game. Fun stuff.",
+		"17titleplus": "A multilingually supported patch that adds a cool big graphic onto WBaWC's stage titles."
 	},
 	"servers": [
 		"https://mirrors.thpatch.net/someguy/"
