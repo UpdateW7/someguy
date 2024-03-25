959a87216f5e70b1fa017c9edcf6a8ee53223b7b
Some Guy in Paradise <tomvilar11937@gmail.com>
2024-03-25 19:29:32 -0400

    lost market bug fix and title plus collection

---
+++ titleplus/patch.js
@@ -0,0 +1,11 @@
+{
+    "id": "titleplus",
+    "title": "A compilation of the titleplus mods. Will maybe also be updated with new games sometime later!",
+    "dependencies": [
+        "nmlgc/base_tsa"
+    ],
+    "update": true,
+    "servers": [
+        "https://mirrors.thpatch.net/someguy/"
+    ]
+}
