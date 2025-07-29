6866476119e9d134423eabef47f12e16af0fa7ab
Some Guy in Paradise <tomvilar11937@gmail.com>
2025-07-29 00:42:45 -0400

    Phantasmagoria of Aspiring Idyll

---
+++ poai/patch.js
@@ -0,0 +1,12 @@
+{
+    "id": "poai",
+    "dependencies": [
+        "nmlgc/base_tsa",
+		"nmlgc/script_latin",
+		"nmlgc/western_name_order"
+    ],
+    "servers": [
+        "https://mirrors.thpatch.net/someguy/"
+    ],
+    "title": "A large-scale mod of UDoALG that gives everyone new attacks and more!"
+}
---
+++ poai/th19/spells.js
@@ -0,0 +1,63 @@
+{
+    "0+Boss00BossCard1+0": "Barrier Sign \"Imperial-Busting Ying-Yang Orb Seal\"",
+    "0+Boss00BossCard1H+0": "Barrier Sign \"Imperial-Busting Ying-Yang Orb Seal\"",
+	"0+BossCard2+0": "Gensou Fantasy \"Illusionary Hakurei Barrier\"",
+    "0+BossCard3+0": "\"Merciless Seal for Exorcizing Demonic Beasts\"",
+	
+    "1+Boss00BossCard1+0": "Light Blast \"Shoot the Underworld\"",
+    "1+Boss00BossCard1H+0": "Light Blast \"Shoot the Underworld\"",
+	
+    "4+Boss00BossCard1+0": "Spinning Top \"Koma-Inu Spin\"",
+    "4+Boss00BossCard1+4": "Spinning Top \"Koma-Inu Giant Slalom Spin\"",
+	
+    // "5+Boss00BossCard1+0": "\"Four Little Field Mice who Dowse the Sky\"",
+    // "5+Boss00BossCard1+4": "\"Four Little Field Mice who Dowse the Sky\"",
+    "5+Boss00BossCard1+0": "Gemstone Rain \"Mouse of Spring Showers\"",
+    "5+Boss00BossCard1+4": "Gemstone Rain \"Mouse of Spring Showers\"",
+	
+    "6+Boss00BossCard1+0": "Arsenal Sign \"Lunarian Interbellum Arms Race\"",
+	
+    "2+Boss00BossCard1+0": "Miracle Sign \"Thwart Malice, Sky Blue Wind!\"",
+    "2+Boss00BossCard1+6": "Miracle Sign \"Thwart Malice, Sky Blue Wind!\"",
+	
+    "3+Boss00BossCard1+0": "Shikigami \"Protection of Zenki and Goki\"",
+    "3+Boss00BossCard1+6": "Shikigami \"Protection of Zenki and Goki\"",
+	
+    "7+Boss00BossCard1+0": "Vengeful Spirit \"Towards the Chariot of Demise\"",
+	
+    "8+BossCard1+0": "Kuda-gitsune \"Vials of Vile Deception\"",
+	
+    //"9+BossCard1+0": "Tanuki Sign \"Blend in with Greedy Beast Spirits!\"",
+	"9+BossCard1+0": "\"Danmaku Transformation of Behavioral Modernity\"",
+	
+    "10+BossCard1+0": "Tortoiseshell Sign \"Jidiao Great Barrier\"",
+    "10+BossCard2+0": "Matriarch Sign \"Otter Oyabun\"",
+    "10+BossCard3+0": "Submission \"Eight-Thousand Scales\"",
+	
+    "11+BossCard1+0": "Heavenly Stellar Steed \"Pegasus Starmine\"",
+    //"11+BossCard2+0": "Heavenly Stellar Steed \"Pegasus Starmine\"",
+	"11+BossCard2+0": "Matriarch Sign \"Wolfpack Domiance\"",
+    "11+BossCard3+0": "\"Last Shoot\"",
+	
+    "12+BossCard1+0": "Old Grudge \"Gorge Riser\"",
+    //"12+BossCard2+0": "\"Absorber Phantom\"",
+	"12+BossCard2+0": "Matriarch Sign \"Quauhteca Eagles\"",
+    "12+BossCard3+0": "\"Gluttony of the Deepest Depths\"",
+	
+    "13+BossCard1+0": "Oni Sign \"Deep Fog Drunk Oni\"",
+    "13+BossCard2+0": "\"Sparsely-Silent Chief General of the Will-o'-wisps\"",
+    "13+BossCard3+0": "\"Beautiful Hell Danmaku Night Parade\"",
+	
+    "14+BossCard1+0": "Monkey Magic \"Self-Proclaimed Yousenjutsu\"",
+	
+    "15+BossCard1+0": "Underworld Sign \"Blazing Heads of Cerberus\"",
+	
+    "16+BossCard1+0": "Former Hell \"Cursed Blood Liquefaction\"",
+	
+    "17+BossCard1+0": "Executor \"Jailbreak Stalker\"",
+	
+    "18+BossCard1+0": "\"Pure Spirit Shot ~Cognizance & Insentience~\"",
+    // "18+BossCard2+0": "\"Insentient Pure Spirit Shot\"",
+	"18+BossCard2+0": "Kuusou Fantasy \"Beast King's Self-Sufficient World\"",
+    "18+BossCard3+0": "\"Kingdom of Lost Sheep\""
+}
